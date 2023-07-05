1fa4ae417f2c61a26f28f9e2e7a5c18797f93248
Artur_Tadevosyan <arturtadevosyan926@gmail.com>
2023-07-05 20:34:25 +0400

    change task 29

--- src/homework/TasksWith3DigitNumbers.java
+++ src/homework/TasksWith3DigitNumbers.java
@@ -4,7 +4,7 @@ public class TasksWith3DigitNumbers {
 
     public static void main(String[] args) {
 
-        int number1 = 5, number2 = -4, number3 = 8;
+        int number1 =20, number2 = 16, number3 = 21;
 
         //Task 24
         System.out.println("------------------------");
@@ -55,7 +55,7 @@ public class TasksWith3DigitNumbers {
             } else {
                 System.out.println(number1 + "->" + number3 + "->" + number2);
             }
-        } else if (number2 < number3) {
+        } else if (number2 < number3 && number2 < number1) {
             if (number1 < number3) {
                 System.out.println(number2 + "->" + number1 + "->" + number3);
             } else {
